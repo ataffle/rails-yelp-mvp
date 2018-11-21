dd85cd4d05d2c7501f1b3b6625a2bab53160c402
Alix Taffle <ataffle@gmail.com>
2018-11-21 15:45:35 +0100

    completed yelp mvp

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,11 +1,12 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [ :show ]
+
 
   def index
     @restaurants = Restaurant.all
   end
 
   def show
-    @restaurant = Restaurant.find(params[:id])
   end
 
   def new
@@ -13,13 +14,22 @@ class RestaurantsController < ApplicationController
   end
 
   def create
-    redirect_to restaurant_path(@restaurant)
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
 private
 
   def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category)
+  end
 
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 
 end
